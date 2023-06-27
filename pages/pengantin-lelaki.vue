e2af4d692f413bfe5a82c7e08db03c83260c66c0
Amirul <naimteehee@gmail.com>
2023-06-27 23:00:25 +0800

    Update pages

--- pages/pengantin-lelaki.vue
+++ pages/pengantin-lelaki.vue
@@ -4,4 +4,100 @@
     });
 </script>
 
-<template>about</template>
+<template>
+    <card-section class="h-screen flex flex-col justify-center">
+        <div class="m-3 px-3 py-32 border-8">
+            <p class="uppercase text-lg">Majlis<br/>Perkahwinan</p>
+            <div class="text-3xl my-6">
+                <p>Amirul Arif</p>
+                <p class="text-center text-sm">dan</p>
+                <p class="text-right">Nurul Nabila</p>
+            </div>
+            <p class="flex flex-row text-center justify-center mb-3">
+                <span class="align-middle my-auto mx-3">Sep</span>
+                <span class="align-middle mx-3 px-3 border-x-4">Sabtu<br/><span class="text-2xl">16</span></span>
+                <span class="align-middle my-auto mx-3">2023</span>
+            </p>
+            <p class="text-center">Seksyen 27, Shah Alam, Selangor.</p>
+        </div>
+    </card-section>
+    <card-section class="text-center">
+        <p>Assalamualaikum W.B.T</p>
+        <p>Hj Mohd Solong Bin Abdul Ghani</p>
+        <p>Hjh Noorma Binti Othman</p>
+        <p>Dengan penuh kesyukuran ke hadrat Ilahi, kami sekeluarga menjemput</p>
+        <p>Dato' / Datin / Tuan / Puan / Encik / Cik / Hj / Hjh</p>
+        <p>sekeluarga hadir ke Majlis Pernikahan anakanda kami</p>
+        <p>Amirul Arif Bin Mohd Sulong</p>
+        <p>dan</p>
+        <p>Nurul Nabila Natasya Binti Abu Bakar</p>
+    </card-section>
+    <card-section class="text-center">
+        <p>Sabtu, 16 September 2023</p>
+        <p>30 Safar 1445h</p>
+        <p>11.30 Am - 4.00 Pm</p>
+        <p>No. 11, Jalan Bukit Rasa 27/81,</p>
+        <p>Seksyen 27,</p>
+        <p>40400, Shah Alam,</p>
+        <p>Selangor.</p>
+    </card-section>
+    <card-section class="text-center">
+        <p>Semoga dengan kehadiran dan doa restu</p>
+        <p>Dato' / Datin / Tuan / Puan / Encik / Cik / Hj / Hjh</p>
+        <p>dapat menyerikan lagi majlis kami dan diberkati Allah SWT</p>
+    </card-section>
+    <card-section class="text-center">
+        <p>Hubungi:</p>
+        <table class="mx-auto">
+            <tbody>
+                <tr>
+                    <td class="text-right">
+                        Sulong :
+                    </td>
+                    <td class="text-left pl-3">
+                        013 383 1228
+                    </td>
+                </tr>
+                <tr>
+                    <td class="text-right">
+                        Noorma :
+                    </td>
+                    <td class="text-left pl-3">
+                        013 219 8298
+                    </td>
+                </tr>
+                <tr>
+                    <td class="text-right">
+                        Anwar :
+                    </td>
+                    <td class="text-left pl-3">
+                        013 262 8238
+                    </td>
+                </tr>
+                <tr>
+                    <td class="text-right">
+                        Naim :
+                    </td>
+                    <td class="text-left pl-3">
+                        019 340 8298
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <p>Turut Mengundang Seisi Keluarga</p>
+    </card-section>
+    <card-section>
+        Google Map
+    </card-section>
+    <card-footer class="text-center">
+        <div class="grid grid-flow-col justify-stretch">
+            <div class="border-2">Kalendar</div>
+            <div class="border-2">Hubungi</div>
+            <div class="border-2">Peta</div>
+            <div class="border-2">Nota</div>
+        </div>
+        <div class="py-1 text-xs">
+            Dihasilkan oleh <a href="https://naimsolong.github.io" target="_blank">Amirul Naim</a>
+        </div>
+    </card-footer>
+</template>
--- pages/pengantin-perempuan.vue
+++ pages/pengantin-perempuan.vue
@@ -4,4 +4,103 @@
     });
 </script>
 
-<template>about</template>
+<template>
+    <card-section class="h-screen flex flex-col justify-center">
+        <div class="m-3 px-3 py-32 border-8 text-center">
+            <p class="uppercase text-lg">Jemputan Majlis</p>
+            <p class="text-xl">Walimatul Urus</p>
+            <div class="text-3xl my-12">
+                <p>Nurul Nabila</p>
+                <p class="text-sm">&</p>
+                <p>Amirul Arif</p>
+            </div>
+            <p>Sabtu</p>
+            <p class="flex flex-row text-center justify-center mb-3">
+                <span>&nbsp;&nbsp;&nbsp;&nbsp;09</span>
+                <span class="mx-3 px-3 border-x-4">September</span>
+                <span>2023</span>
+            </p>
+            <p>Sofea Guest House<br/>Taman Bukit Dahlia, Pasir Gudang, Johor.</p>
+        </div>
+    </card-section>
+    <card-section class="text-center">
+        <p>Assalamualaikum W.B.T</p>
+        <p>Abu Bakar Bin Ahmad</p>
+        <p>Rafidah Binti Mohd Sharif</p>
+        <p>Dengan penuh kesyukuran ke hadrat Ilahi, kami sekeluarga menjemput</p>
+        <p>Dato' / Datin / Tuan / Puan / Encik / Cik / Hj / Hjh</p>
+        <p>sekeluarga hadir ke Majlis Pernikahan anakanda kami</p>
+        <p>Nurul Nabila Natasya Binti Abu Bakar</p>
+        <p>dan</p>
+        <p>Amirul Arif Bin Mohd Sulong</p>
+    </card-section>
+    <card-section class="text-center">
+        <p>Sabtu, 09 September 2023</p>
+        <p>23 Safar 1445h</p>
+        <p>11.30 Am - 4.00 Pm</p>
+        <p>Sofea Guest House</p>
+        <p>No 6, Jalan Dahlia 4,</p>
+        <p>Taman Bukit Dahlia,</p>
+        <p>81700 Pasir Gudang,</p>
+        <p>Johor.</p>
+    </card-section>
+    <card-section class="text-center">
+        <p>Semoga dengan kehadiran dan doa restu</p>
+        <p>Dato' / Datin / Tuan / Puan / Encik / Cik / Hj / Hjh</p>
+        <p>dapat menyerikan lagi majlis kami dan diberkati Allah SWT</p>
+    </card-section>
+    <card-section class="text-center">
+        <p>Hubungi:</p>
+        <table class="mx-auto">
+            <tbody>
+                <tr>
+                    <td class="text-right">
+                        Abu Bakar :
+                    </td>
+                    <td class="text-left pl-3">
+                        018 665 3059
+                    </td>
+                </tr>
+                <tr>
+                    <td class="text-right">
+                        Rafidah :
+                    </td>
+                    <td class="text-left pl-3">
+                        011 5754 2732
+                    </td>
+                </tr>
+                <tr>
+                    <td class="text-right">
+                        Hariz :
+                    </td>
+                    <td class="text-left pl-3">
+                        018 222 0431
+                    </td>
+                </tr>
+                <tr>
+                    <td class="text-right">
+                        Kamarul :
+                    </td>
+                    <td class="text-left pl-3">
+                        018 662 3321
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <p>Turut Mengundang Seisi Keluarga</p>
+    </card-section>
+    <card-section>
+        Google Map
+    </card-section>
+    <card-footer class="text-center">
+        <div class="grid grid-flow-col justify-stretch">
+            <div class="border-2">Kalendar</div>
+            <div class="border-2">Hubungi</div>
+            <div class="border-2">Peta</div>
+            <div class="border-2">Nota</div>
+        </div>
+        <div class="py-1 text-xs">
+            Dihasilkan oleh <a href="https://naimsolong.github.io" target="_blank">Amirul Naim</a>
+        </div>
+    </card-footer>
+</template>
