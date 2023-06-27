d71eb417b65023abc68418c2bb2b239f80ec9d3e
Amirul <naimteehee@gmail.com>
2023-06-27 10:11:45 +0800

    Add components

---
+++ components/card.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="h-full w-full p-8 drop-shadow-md bg-gray-50 rounded-2xl overflow-hidden hover:drop-shadow-xl hover:bg-gray-100 hover:p-0">
+        <slot />
+    </div>
+</template>
