d5ad8733d31ffb204a034033a8a4bf33a4416a9a
Amirul <naimteehee@gmail.com>
2023-06-28 13:42:45 +0800

    Setup tailwindcss config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,14 @@ export default defineNuxtConfig({
   modules: [
     '@nuxtjs/tailwindcss'
   ],
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  tailwindcss: {
+    cssPath: '~/assets/css/tailwind.css',
+    configPath: 'tailwind.config.js',
+    exposeConfig: false,
+    exposeLevel: 2,
+    config: {},
+    injectPosition: 'first',
+    viewer: true,
+  }
 })
