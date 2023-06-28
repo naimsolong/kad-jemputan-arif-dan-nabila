193e9184ed1b3709fa64c7bf75a650b2eda77813
Amirul <naimteehee@gmail.com>
2023-06-28 09:26:47 +0800

    Update footer

--- components/card-footer-note.vue
+++ components/card-footer-note.vue
@@ -1,8 +1,8 @@
 <template>
-    <div>
+    <div class="text-white">
         Nota
         <div class="py-1 text-xs">
-            Dihasilkan oleh <a href="https://naimsolong.github.io" target="_blank">Amirul Naim</a>
+            Dihasilkan oleh <a href="https://naimsolong.github.io" target="_blank" class="hover:font-bold underline">Amirul Naim</a>
         </div>
     </div>
 </template>
