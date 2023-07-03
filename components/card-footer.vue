ef30d24dab5ea7fd3a3a8df2a5027379048f3b2b
Amirul <naimteehee@gmail.com>
2023-07-03 10:25:38 +0800

    Test again

--- components/card-footer.vue
+++ components/card-footer.vue
@@ -13,7 +13,7 @@ const changeTab = (name) => {
 </script>
 
 <template>
-    <div class="text-center sticky bottom-0 min-w-96 max-w-lg max-w-fit mx-auto drop-shadow-xl bg-[#343a40]">
+    <div class="text-center sticky bottom-0 bg-[#343a40]">
         <div :class="{'hidden': tab == '','max-h-96 p-6 mb-5': true}">
             <div class="absolute min-w-max min-h-max top-0 right-0 mr-7 -mt-2 cursor-pointer bg-[#282928] fill-white rounded-full" @click="changeTab('')">
                 <svg class="stroke-2 stroke-[#282928] scale-[2] drop-shadow-lg" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z"/></svg>
