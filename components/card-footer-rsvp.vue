a1ee92d543b3c58cf9d1ea085e077f78eb0800f8
Amirul <naimteehee@gmail.com>
2023-07-09 14:36:01 +0800

    Update rsvp heights

--- components/card-footer-rsvp.vue
+++ components/card-footer-rsvp.vue
@@ -8,6 +8,6 @@ const props = defineProps({
 <template>
     <div class="relative text-white w-full h-full">
         <p class="text-xl mb-9 uppercase font-bold">RSVP</p>
-        <iframe :src="props.rsvp.iframe" style="border:0; width: 100%; height: 600px; margin: 0;" frameborder="0" marginheight="0" marginwidth="0" allowfullscreen="false" loading="lazy" referrerpolicy="no-referrer-when-downgrade">Loading…</iframe>
+        <iframe :src="props.rsvp.iframe" style="border:0; width: 100%; height: 70vh; margin: 0;" frameborder="0" marginheight="0" marginwidth="0" allowfullscreen="false" loading="lazy" referrerpolicy="no-referrer-when-downgrade">Loading…</iframe>
     </div>
 </template>
